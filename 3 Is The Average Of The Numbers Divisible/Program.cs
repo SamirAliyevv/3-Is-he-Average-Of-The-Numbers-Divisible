e4364941917e83f2447a76780c937c78001d692b
SamirAliyevv <samirsa@code.edu.az>
2023-01-16 14:30:27 -0800

    master

---
+++ 3 Is The Average Of The Numbers Divisible/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3_Is_The_Average_Of_The_Numbers_Divisible
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 66, 35, 56, 7, 8, 21, 99 };
+            int  count = 0;
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 3==0)
+                {
+                    sum+= numbers[i];
+                    count++;
+                }
+
+            }
+            Console.WriteLine(sum / count);
+        }     
+    }
+}
